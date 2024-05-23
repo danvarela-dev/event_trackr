1dace4d55124e4e78f4744a5aca6b6e0b8ae3336
Kevs98 <kevinpalma657@gmail.com>
2024-05-22 22:11:30 -0600

    correct syntax

--- apps/server/config/swagger.config.ts
+++ apps/server/config/swagger.config.ts
@@ -3,8 +3,5 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Event Tracker API')
   .setVersion('1.0')
-  .addServer(
-    'http://http://vps-96efac4c.vps.ovh.ca/:3000/',
-    'Local Development',
-  )
+  .addServer('http://vps-96efac4c.vps.ovh.ca:3000/', 'Local Development')
   .build();
