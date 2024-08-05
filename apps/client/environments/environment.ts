03d911a3be5d082e8186eaacecb9bced45072eb2
Daniel Varela <daniel38912@gmail.com>
2024-08-05 00:03:29 -0600

    update environment

--- apps/client/environments/environment.ts
+++ apps/client/environments/environment.ts
@@ -1,9 +1,8 @@
-// export const environment = {
-//   api: 'http://vps-96efac4c.vps.ovh.ca:3000/api',
-//   jwt_secret:
-//     '3d389a3051558ba0a417f1ce5e35f13aa0d2f6d657428c52325e75b436504b03',
-// };
-
 export const environment = {
-  api: 'http://localhost:3000/api',
+  api: 'http://vps-96efac4c.vps.ovh.ca:3000/api',
+  jwt_secret:
+    '3d389a3051558ba0a417f1ce5e35f13aa0d2f6d657428c52325e75b436504b03',
 };
+
+// export const environment = {
+//   api: 'http://localhost:3000/api',// };
