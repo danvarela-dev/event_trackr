ee4d9162942268942a85a21b6c3c9a927f0e38f7
Kevs98 <kevinpalma657@gmail.com>
2024-09-20 22:09:07 -0600

    change vault interface

--- apps/client/src/app/components/vault/vault-card/vault-card.component.ts
+++ apps/client/src/app/components/vault/vault-card/vault-card.component.ts
@@ -10,6 +10,7 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { MessageService } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
 @Component({
   selector: 'event-trackr-vault-card',
   standalone: true,
@@ -23,6 +24,7 @@ import { MessageService } from 'primeng/api';
     IconFieldModule,
     InputIconModule,
     ButtonModule,
+    AvatarModule,
   ],
   templateUrl: './vault-card.component.html',
   styleUrl: './vault-card.component.scss',
@@ -32,7 +34,20 @@ export class VaultCardComponent {
   @Output() requestPassword = new EventEmitter<number>();
   @Output() delete = new EventEmitter<{ id: number; event: Event }>();
   messageService = inject(MessageService);
-
+  logoColors: Record<string, string> = {
+    APPLE: 'linear-gradient(135deg, #000000, #555555)',
+    TIKTOK: 'linear-gradient(135deg, #ff0050, #00f2ea, #000000)',
+    FACEBOOK: 'linear-gradient(135deg, #1877f2, #898F9C)',
+    YOUTUBE: 'linear-gradient(135deg, #FF0000, #282828)',
+    INSTAGRAM:
+      'linear-gradient(135deg, #405DE6, #5B51D8, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #777737, #FCAF45, #FFDC80)',
+    EMAIL: 'linear-gradient(135deg, #405DE6, #5B51D8)',
+    WORDPRESS: 'linear-gradient(135deg, #21759b, #000000)',
+    TUMBLR: 'linear-gradient(135deg, #34526f, #000000)',
+    MEGA: 'linear-gradient(135deg, #FF0000, #000000)',
+    DROPBOX: 'linear-gradient(135deg, #3d9ae8, #000000)',
+    TWITTER: 'linear-gradient(135deg, #000000, #555555)',
+  };
   copyUsername(): void {
     navigator.clipboard.writeText(this.vault.username);
 
@@ -52,5 +67,11 @@ export class VaultCardComponent {
     if (id) {
       this.delete.emit({ id, event });
     }
+  }
+
+  getGradientColor(category: string): string {
+    return (
+      this.logoColors[category] || 'linear-gradient(135deg, #ffffff, #000000)'
+    );
   }
 }
